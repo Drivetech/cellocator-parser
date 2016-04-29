0367d472c5e2b8605d401483d26987095a105cca
Leonardo Gatica <lgatica@protonmail.com>
2016-04-29 17:44:47 -0300

    Remove gpsTime

--- test/test.js
+++ test/test.js
@@ -14,7 +14,6 @@ describe('cellocator-parzer', () => {
     expect(data.loc.coordinates).to.eql([-79.09097658351084, -7.953307941260071]);
     expect(data.speed).to.eql(84.96);
     expect(data.datetime).to.eql('2016-03-12T23:42:00.000Z');
-    expect(data.gpsTime).to.eql('2016-04-25T18:10:00.000Z');
     expect(data.direction).to.eql(155.09967514191385);
     expect(data.satellites).to.eql(9);
     expect(data.voltage.ada).to.eql(28.1176470588165);
