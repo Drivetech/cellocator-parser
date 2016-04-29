444790da8d0e8d9c41985f0168a490b8fd2ca0ef
Leonardo Gatica <lgatica@protonmail.com>
2016-04-29 17:44:56 -0300

    Refactor

---
+++ src/hardwares.js
@@ -0,0 +1,207 @@
+module.exports = [
+  {
+    id: 1,
+    model: {id: 1, name: 'CR300'},
+    modem: {code: 7, name: 'GE864-QUAD-V2'}
+  }, {
+    id: 2,
+    model: {id: 2, name: 'CFE'},
+    modem: {code: 0, name: 'No Modem'}
+  }, {
+    id: 194,
+    model: {id: 2, name: 'PHSN RF Head'},
+    modem: {code: 6, name: 'No Modem'}
+  }, {
+    id: 35,
+    model: {id: 3, name: 'Olympic'},
+    modem: {code: 1, name: 'No Modem - Tetra/Astro external modem'}
+  }, {
+    id: 4,
+    model: {id: 4, name: 'Compact Fleet'},
+    modem: {code: 0, name: 'Sony/Erickson GR47'}
+  }, {
+    id: 36,
+    model: {id: 4, name: 'Compact Fleet'},
+    modem: {code: 1, name: 'Enfora Enabler II-G'}
+  }, {
+    id: 68,
+    model: {id: 4, name: 'Compact Fleet'},
+    modem: {code: 2, name: 'Telit GE864, old retrofit board(obsolete)'}
+  }, {
+    id: 100,
+    model: {id: 4, name: 'Compact Fleet'},
+    modem: {code: 3, name: 'Telit GE864, mute support'}
+  }, {
+    id: 5,
+    model: {id: 5, name: 'Compact Security'},
+    modem: {code: 0, name: 'Sony/Erickson GR47'}
+  }, {
+    id: 37,
+    model: {id: 5, name: 'Compact Security'},
+    modem: {code: 1, name: 'Enfora Enabler II-G'}
+  }, {
+    id: 69,
+    model: {id: 5, name: 'Compact Security'},
+    modem: {code: 2, name: 'Telit GE864, old retrofit board(obsolete)'}
+  }, {
+    id: 101,
+    model: {id: 5, name: 'Compact Security'},
+    modem: {code: 3, name: 'Telit GE864, mute support'}
+  }, {
+    id: 39,
+    model: {id: 7, name: 'Compact CAN 8 Sensor'},
+    modem: {code: 1, name: 'Enfora Enabler II-G'}
+  }, {
+    id: 71,
+    model: {id: 7, name: ''},
+    modem: {code: 2, name: 'Telit GE864, old retrofit board(obsolete)'}
+  }, {
+    id: 199,
+    model: {id: 7, name: 'Compact CAN TOB'},
+    modem: {code: 6, name: 'Telit GE864, automotive'}
+  }, {
+    id: 8,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit JF2 GPS)'},
+    modem: {code: 0, name: 'Cinterion BGS2-W.Rel2'}
+  }, {
+    id: 40,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit JF2 GPS)'},
+    modem: {code: 1, name: 'Cinterion EHS5-E'}
+  }, {
+    id: 72,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit JF2 GPS)'},
+    modem: {code: 2, name: 'Cinterion EHS5-US'}
+  }, {
+    id: 136,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit SE868-V2 GNSS)'},
+    modem: {code: 4, name: 'Cinterion BGS2-W.Rel2'}
+  }, {
+    id: 168,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit SE868-V2 GNSS)'},
+    modem: {code: 5, name: 'Cinterion EHS5-E'}
+  }, {
+    id: 200,
+    model: {id: 8, name: 'CelloTrack nano 10 (Telit SE868-V2 GNSS)'},
+    modem: {code: 6, name: 'Cinterion EHS5-US'}
+  }, {
+    id: 9,
+    model: {id: 9, name: '370-50'},
+    modem: {code: 0, name: 'Sony/Erickson GR47'}
+  }, {
+    id: 105,
+    model: {id: 9, name: '370-50'},
+    modem: {code: 3, name: 'Telit GE864, mute support'}
+  }, {
+    id: 170,
+    model: {id: 10, name: 'CelloTrack 1 Output'},
+    modem: {code: 5, name: 'Enfora 3'}
+  }, {
+    id: 235,
+    model: {id: 11, name: 'CR300B'},
+    modem: {code: 7, name: 'GE864-QUAD-V2'}
+  }, {
+    id: 172,
+    model: {id: 12, name: 'CelloTrack'},
+    modem: {code: 5, name: 'Enfora III'}
+  }, {
+    id: 78,
+    model: {id: 14, name: 'Cello-IQ GNSS'},
+    modem: {code: 2, name: 'GE910 QUAD V3'}
+  }, {
+    id: 209,
+    model: {id: 17, name: 'Compact CAN TOB'},
+    modem: {code: 6, name: 'Telit GE864, automotive'}
+  }, {
+    id: 18,
+    model: {id: 18, name: 'CelloTrack T (2G)'},
+    modem: {code: 0, name: 'Telit GE910 QUAD (v2) (V3)'}
+  }, {
+    id: 82,
+    model: {id: 18, name: 'CelloTrack T (3G)'},
+    modem: {code: 2, name: 'Telit HE910 NAD'}
+  }, {
+    id: 19,
+    model: {id: 19, name: 'CelloTrackPower T (2G)'},
+    modem: {code: 0, name: 'Telit GE910 QUAD (v2) (V3)'}
+  }, {
+    id: 83,
+    model: {id: 19, name: 'CelloTrackPower T (3G)'},
+    modem: {code: 2, name: 'Telit HE910 NAD'}
+  }, {
+    id: 20,
+    model: {id: 20, name: 'Cello-CANiQ (NA)'},
+    modem: {code: 0, name: 'UE910 NAR'}
+  }, {
+    id: 52,
+    model: {id: 20, name: 'Cello-CANiQ (EU)'},
+    modem: {code: 1, name: 'UE910 EUR'}
+  }, {
+    id: 84,
+    model: {id: 20, name: 'Cello-CANiQ (2G)'},
+    modem: {code: 2, name: 'GE910 QUAD V3'}
+  }, {
+    id: 182,
+    model: {id: 22, name: 'compact EOB'},
+    modem: {code: 5, name: 'Enfora III'}
+  }, {
+    id: 183,
+    model: {id: 23, name: 'CelloTrack Power'},
+    modem: {code: 5, name: 'Enfora III'}
+  }, {
+    id: 216,
+    model: {id: 24, name: 'Cello (Telit)'},
+    modem: {code: 6, name: 'Telit GE864, automotive'}
+  }, {
+    id: 249,
+    model: {id: 25, name: 'Cello Cinterion'},
+    modem: {code: 7, name: 'Cinterion BGS3'}
+  }, {
+    id: 26,
+    model: {id: 26, name: 'Cello Cinterion'},
+    modem: {code: 0, name: 'Cinterion BGS2-W.Rel2'}
+  }, {
+    id: 58,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit SE868-V2 GNSS)'},
+    modem: {code: 1, name: 'Cinterion EHS5-E'}
+  }, {
+    id: 90,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit SE868-V2 GNSS)'},
+    modem: {code: 2, name: 'Cinterion EHS5-US'}
+  }, {
+    id: 122,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit SE868-V2 GNSS)'},
+    modem: {code: 3, name: 'Cinterion EHS6'}
+  }, {
+    id: 154,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit JF2 GPS)'},
+    modem: {code: 4, name: 'Cinterion BGS2-W.Rel2'}
+  }, {
+    id: 186,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit JF2 GPS)'},
+    modem: {code: 5, name: 'Cinterion EHS5-E'}
+  }, {
+    id: 218,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit JF2 GPS)'},
+    modem: {code: 6, name: 'Cinterion EHS5-US'}
+  }, {
+    id: 250,
+    model: {id: 26, name: 'CelloTrack nano 20 (Telit JF2 GPS)'},
+    modem: {code: 7, name: 'Cinterion EHS6'}
+  }, {
+    id: 220,
+    model: {id: 28, name: '(Compact (TOB)) in use'},
+    modem: {code: 6, name: 'Telit GE864, automative'}
+  }, {
+    id: 221,
+    model: {id: 29, name: 'CR200'},
+    modem: {code: 6, name: 'Telit GE864, automative'}
+  }, {
+    id: 223,
+    model: {id: 30, name: 'CR200B'},
+    modem: {code: 6, name: 'Telit GE864, automative'}
+  }, {
+    id: 224,
+    model: {id: 31, name: 'Cello-IQ'},
+    modem: {code: 6, name: 'Telit GE864, automative'
+  }}
+];
---
+++ src/utils.js
@@ -0,0 +1,254 @@
+const ConvertBase = require('convert-base');
+const hardwares = require('./hardwares');
+const moment = require('moment');
+const pad = require('pad');
+
+const convertBase = (data, fromBase , toBase) => {
+  const converter = new ConvertBase();
+  return converter.convert(data, fromBase, toBase);
+};
+
+const hex2Ascii = (data) => data.split(/([A-F0-9]{2})/i).filter(x => x !== '').map(x => String.fromCharCode(parseInt(x, 16))).join('');
+
+const reverseHex = (data) => {
+  data = data.toUpperCase();
+  if (data.length % 2 !== 0) data = `0${data}`;
+  return data.split(/([A-F0-9]{2})/).filter(x => x !== '').reverse().join('');
+};
+
+const lpad = (data, length) => pad(length, data, '0');
+
+const hex2bin = (data) => lpad(convertBase(data, 16, 2), data.length * 4);
+
+exports.parseSystemCode = (bytes1To4) => hex2Ascii(bytes1To4);
+
+exports.parseMessageType = (byte5) => convertBase(byte5, 16, 10);
+
+exports.parseUnitsId = (bytes6To9) => convertBase(reverseHex(bytes6To9), 16, 10);
+
+exports.parseCommunication = (bytes10To11) => {
+  const byte10 = hex2bin(bytes10To11.substring(0, 2));
+  const byte11 = hex2bin(bytes10To11.substring(2, 4));
+  return {
+    garminDisabled: byte11[0],
+    garminNotConnected: byte11[1],
+    directFromRam: byte11[2],
+    pspModeIsEnabled: byte11.substring(3, 5),
+    notCanOriginatedSpeed: byte11[5],
+    notCanOriginatedOdometer: byte11[6],
+    activeTransmission: byte11[7],
+    noHibernation: byte10[0],
+    momentarySpeed: byte10[1],
+    h: byte10.substring(3, 7)
+  };
+};
+
+exports.parseMessageNumerator = (byte12) => convertBase(byte12, 16, 10);
+
+const parseHardware = (byte13) => {
+  const data = hex2bin(byte13);
+  const modem  = convertBase(data.substr(0, 3), 2, 10);
+  const model = convertBase(data.substr(3, 5), 2, 10);
+  const hardware = hardwares.find(x => x.model.id === model && x.modem.code === modem);
+  return hardware ? hardware.id : '';
+};
+
+const parseSoftware = (byte14) => convertBase(byte14, 16, 10);
+
+exports.parseVersion = (byte13, byte14) => `HW: <${parseHardware(byte13)}>, SW: <${parseSoftware(byte14)}>`;
+
+exports.protocolVersionIdentifier = (byte15) => convertBase(byte15, 16, 10);
+
+exports.parseUnitsStatusCurrentGsmOperator = (byte16) => {
+  const firstnibble = byte16[0];
+  const unitsstatus = convertBase(byte16[1], 16, 2);
+  return {
+    speedEstimatedByGps: unitsstatus[0] === '1',
+    correctTime: unitsstatus[1] === '1',
+    homeNetwork: unitsstatus[2] === '1',
+    gpsCommunicationAvailable: unitsstatus[3] === '1',
+    currentGsmOperator1stNibble: firstnibble
+  };
+};
+
+exports.parseCurrentGsmOperator = (byte17) => byte17;
+
+exports.parseTransmissionReasonSpecificData = (byte18) => convertBase(byte18, 16, 10);
+
+exports.parseTransmissionReason = (byte19) => convertBase(byte19, 16, 10);
+
+exports.parseEngineStatus = (byte20) => convertBase(byte20, 16, 10);
+
+exports.parseIO = (bytes21To24) => {
+  const byte21 = hex2bin(bytes21To24.substring(0, 2));
+  const byte22 = hex2bin(bytes21To24.substring(2, 4));
+  const byte23 = hex2bin(bytes21To24.substring(4, 6));
+  const byte24 = hex2bin(bytes21To24.substring(6, 8));
+  return {
+    unlockInactive: byte21[0] === '1',
+    panicInactive: byte21[1] === '1',
+    drivingStatus: byte21[2] === '1',
+    shockInactive: byte21[6] === '1',
+    doorInactive: byte21[7] === '1',
+    ignitionPortStatus: byte22[0] === '1',
+    accelerometerStatus: byte22[1] === '1',
+    lock: byte22[5] === '1',
+    gpsPower: byte23[4] === '1',
+    gradualStop: byte23[5] === '1',
+    siren: byte23[6] === '1',
+    charge: byte24[0] === '1',
+    standardImmobilizer: byte24[2] === '1',
+    globalOutput: byte24[4] === '1',
+    ledOut: byte24[7] === '1'
+  };
+};
+
+exports.parsePlmn = (byte25, currentGsmOperator1stNibble, currentGsmOperator) => {
+  return convertBase(`${currentGsmOperator1stNibble}${currentGsmOperator}${byte25}`, 16, 10);
+};
+
+exports.parseAnalogInput1 = (byte26) => convertBase(byte26, 16, 10) * 0.1176470588235;
+
+exports.parseAnalogInput2 = (byte27) => convertBase(byte27, 16, 10) * 0.01647058823;
+
+exports.parseAnalogInput3 = (byte28) => (convertBase(byte28, 16, 10) * 0.4314) - 40;
+
+exports.parseAnalogInput4 = (byte29) => convertBase(byte29, 16, 10);
+
+exports.parseMileageCounter = (bytes30To32) => convertBase(reverseHex(bytes30To32), 16, 10);
+
+exports.multiPurposeField = (bytes33To38) => reverseHex(bytes33To38);
+
+exports.parseGpsTime = (bytefrom39to40, seconds) => {
+  seconds = lpad(convertBase(seconds, 16, 10), 2);
+  const now = moment().format('YYYY-MM');
+  const data = hex2bin(reverseHex(bytefrom39to40));
+  const day = `00${convertBase(data.substr(0, 5), 2, 10)}`.substr(-2);
+  const hours = `00${convertBase(data.substr(5, 5), 2, 10)}`.substr(-2);
+  const minutes = `00${convertBase(data.substr(10, 6), 2, 10)}`.substr(-2);
+  return moment(`${now}-${day} ${hours}:${minutes}:${seconds}`).toISOString();
+};
+
+exports.parseLocationStatus = (byte41) => hex2bin(byte41);
+
+/*
+With Normal PMODE Filter settings, PMODE valid is 2 to 6
+With Tide PMODE Filter settings PMODE valid 3 or 4
+*/
+exports.parseMode1 = (byte42) => {
+  const pModes = {
+    '0': 'No navigation solution',
+    '1': '1 satellite solution',
+    '2': '2 satellite solution',
+    '3': '3 satellite solution',
+    '4': '>3 satellite solution',
+    '5': '2D point solution (Least square)',
+    '6': '3D point solution (Least square)',
+    '7': 'Dead reckoning'
+  };
+  const tpModes = {
+    '0': 'Full power position',
+    '1': 'Trickle power position'
+  };
+  const altModes = {
+    '0': 'No altitude hold',
+    '1': 'Altitude used from filter',
+    '2': 'Altitude used from user',
+    '3': 'Force altitude (from user)'
+  };
+  const dopMasks = {
+    '0': 'DOP mask not exceeded',
+    '1': 'DOP mask exceeded'
+  };
+  const dgpss = {
+    '0': 'No DGPS position',
+    '1': 'DGPS position'
+  };
+  const {
+    dgps, dopMask, altMode, tpMode, pMode
+  } = /([01]{1})([01]{1})([01]{2})([01]{1})([01]{3})/i.exec(hex2bin(byte42));
+  return {
+    pMode: pModes[pMode],
+    tpMode: tpModes[tpMode],
+    altMode: altModes[altMode],
+    dopMask: dopMasks[dopMask],
+    dgps: dgpss[dgps]
+  };
+};
+
+exports.parseMode2 = (byte43) => {
+  const modes = {
+    '00': 'Solution not validated',
+    '01': 'DR Sensor Data',
+    '02': 'Validated',
+    '04': 'Dead Reckoning',
+    '08': 'Output edited by UI',
+    '10': 'Reserved',
+    '20': 'Reserved',
+    '40': 'Reserved',
+    '80': 'Reserved'
+  };
+  return byte43 in modes ? modes[byte43] : null;
+};
+
+exports.parseSatellites = (byte44) => convertBase(byte44, 16, 10);
+
+const notBinaryData = (data) => data.split('').map(x => x === '1' ? '0' : '1').join('');
+
+const parseLatitude = (bytes45To48) => {
+  let lat = 0;
+  const data = reverseHex(bytes45To48);
+  if (data[0] === 'F') {
+    lat = convertBase(notBinaryData(hex2bin(data)), 2, 10);
+    lat = (lat + 1) * -1;
+  } else {
+    lat = convertBase(data, 16, 10);
+  }
+  return lat * (180 / Math.PI) * Math.pow(10, -8);
+};
+
+const parseLongitude = (bytes49To52) => {
+  let lng = 0;
+  const data = reverseHex(bytes49To52);
+  if (data[0] === 'F') {
+    lng = convertBase(notBinaryData(hex2bin(data)), 2, 10);
+    lng = (lng + 1) * -1 * (180 / Math.PI) * Math.pow(10, -8);
+  } else {
+    lng = convertBase(data, 16, 10);
+    lng = lng * (180 / Math.PI) * Math.pow(10, -9);
+  }
+  return lng;
+};
+
+exports.parseLoc = (bytes45To48, bytes49To52) => {
+  return {
+    type: 'Point',
+    coordinates: [parseLongitude(bytes45To48), parseLatitude(bytes49To52)]
+  };
+};
+
+exports.parseAltitude = (bytes53To56) => convertBase(reverseHex(bytes53To56), 16, 10) * 0.01;
+
+exports.parseSpeed = (bytes57To60) => convertBase(reverseHex(bytes57To60), 16, 10) * 0.036;
+
+exports.parseDirection = (bytes61To62) => convertBase(reverseHex(bytes61To62), 16, 10) * (180 / Math.PI) * 0.001;
+
+exports.parseDatetime = (bytes68To69, byte67, byte66, byte65, byte64, byte63) => {
+  const year = convertBase(reverseHex(bytes68To69), 16, 10);
+  const month = lpad(convertBase(byte67, 16, 10), 2);
+  const day = lpad(convertBase(byte66, 16, 10), 2);
+  const hour = lpad(convertBase(byte65, 16, 10), 2);
+  const minute = lpad(convertBase(byte64, 16, 10), 2);
+  const second = lpad(convertBase(byte63, 16, 10), 2);
+  const date = `${year}${month}${day}${hour}${minute}${second}`;
+  return moment(`${date}+00:00`, 'YYYYMMDDHHmmssZZ').toISOString();
+};
+
+const checksum = (trama) => {
+  const code = trama.split(/([A-F0-9]{2})/i).filter(x => x !== '').reduce((prev, curr) => prev + convertBase(curr, 16, 10), 0);
+  return `00${convertBase(code, 10, 16)}`.substr(-2);
+};
+
+exports.validate = (byte70, bytes4To69) => {
+  return checksum(bytes4To69) === byte70;
+};
